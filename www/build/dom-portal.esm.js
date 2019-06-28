c5439b16d545097044e89d3c0ce65244d45ed769
natemoo-re <ntmoore@knox.edu>
2019-06-27 22:01:27 -0500

    ignore www

--- www/build/dom-portal.esm.js
+++
@@ -1,6 +0,0 @@
-import { a as patchBrowser, b as globals, c as bootstrapLazy } from './dom-portal-4fd39a76.js';
-
-patchBrowser().then(resourcesUrl => {
-  globals();
-  return bootstrapLazy([["dom-portal",[[1,"dom-portal",{"portalId":[1538,"portal-id"],"container":[1],"projected":[516],"hosted":[4]}]]]], { resourcesUrl });
-});
